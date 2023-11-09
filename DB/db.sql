8ae55ae671c67e5cdda39d6127426e231a69ab41
Peter2510 <pedrogordillo202031683@cunoc.edu.gt>
2023-11-09 01:38:19 -0600

    Mapeo de la base de datos

--- DB/db.sql
+++ DB/db.sql
@@ -1,12 +1,11 @@
 CREATE DATABASE parque;
 
-CREATE USER 'adminstracionParque'@'%' IDENTIFIED BY 'Teori@sistemas1';
+CREATE USER 'administracionParque'@'localhost' IDENTIFIED BY 'Teori@sistemas1';
 
-GRANT ALL PRIVILEGES ON parque.* TO 'adminstracionParque'@'%';
-
-ALTER USER 'adminstracionParque'@'%' IDENTIFIED WITH mysql_native_password BY 'Teori@sistemas1';
+GRANT ALL PRIVILEGES ON parque.* TO 'administracionParque'@'localhost' WITH GRANT OPTION;
 
 FLUSH PRIVILEGES;
+
 USE parque;
 
 CREATE TABLE rol(
@@ -16,8 +15,9 @@ CREATE TABLE rol(
 );
 
 CREATE TABLE empleado(
-	id_empleado INT NOT NULL AUTO_INCREMENT,
+	idEmpleado INT NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(75) NOT NULL,
+    usuario VARCHAR(75) NOT NULL UNIQUE,
     rol INT NOT NULL,
     contrasenia BLOB NOT NULL,
     PRIMARY KEY(idEmpleado)
@@ -31,13 +31,13 @@ ALTER TABLE empleado
 
 CREATE TABLE cliente(
     idCliente INT NOT NULL AUTO_INCREMENT,
-	nit VARCHAR(10) NOT NULL,
+	nit VARCHAR(10) NOT NULL UNIQUE,
     nombre VARCHAR(75) NOT NULL,
-    estado_suscripcion BOOLEAN NOT NULL,
-    fecha_inicio_pago DATE NOT NULL,
-    tipo_cliente VARCHAR(2) NOT NULL,
+    estadoSuscripcion BOOLEAN NOT NULL,
+    fechaInicioPago DATE NOT NULL,
+    tipoCliente VARCHAR(2) NOT NULL,
     ubicacion VARCHAR(75) NOT NULL,
-    PRIMARY KEY(nit)
+    PRIMARY KEY(idCliente)
 );
 
 CREATE TABLE area(
@@ -46,20 +46,20 @@ CREATE TABLE area(
     precio DECIMAL(9,2) NOT NULL,
     estado BOOLEAN NOT NULL,
     capacidad INT NOT NULL,
-    hora_inicio TIME NOT NULL,
-    hora_fin TIME NOT NULL,
+    horaInicio TIME NOT NULL,
+    horaFin TIME NOT NULL,
     PRIMARY KEY(idArea)
 );
 
 CREATE TABLE anuncios(
-    id INT NOT NULL AUTO_INCREMENT,
+    idAnuncio INT NOT NULL AUTO_INCREMENT,
     titulo VARCHAR(200) NOT NULL,
     descripcion TEXT NOT NULL,
-    fecha_inicio DATE NOT NULL,
-    fecha_fin DATE NOT NULL,
+    fechaInicio DATE NOT NULL,
+    fechaFin DATE NOT NULL,
     idEmpleado INT NOT NULL,
     idArea INT NOT NULL,
-    PRIMARY KEY(id)
+    PRIMARY KEY(idAnuncio)
 );
 
 ALTER TABLE anuncios 
@@ -70,23 +70,53 @@ ALTER TABLE anuncios
   ADD CONSTRAINT `fk_ID_EMPLEADO_union` FOREIGN KEY (`idEmpleado`) REFERENCES `empleado` (`idEmpleado`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_ID_AREA_union` FOREIGN KEY (`idArea`) REFERENCES `area` (`idArea`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-CREATE TABLE reservas(
-    id INT NOT NULL AUTO_INCREMENT,
-    nit_cliente VARCHAR(10) NOT NULL,
-    id_area VARCHAR(10) NOT NULL,
+CREATE TABLE reserva(
+    idReserva INT NOT NULL AUTO_INCREMENT,
+    nitCliente VARCHAR(10) NOT NULL,
+    idArea INT NOT NULL,
     fecha_reserva DATE NOT NULL,
     fecha_fin_reserva DATE NOT NULL,
-    descripcion TEXT NOT NULL,
-
     idEmpleado INT NOT NULL,
-    idArea INT NOT NULL,
-    PRIMARY KEY(id)
+    PRIMARY KEY(idReserva)
 );
 
-ALTER TABLE a 
+ALTER TABLE reserva
     ADD KEY `fk_ID_EMPLEADO` (`idEmpleado`),
     ADD KEY `fk_ID_AREA` (`idArea`);
 
-ALTER TABLE anuncios
-  ADD CONSTRAINT `fk_ID_EMPLEADO_union` FOREIGN KEY (`idEmpleado`) REFERENCES `empleado` (`idEmpleado`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_ID_AREA_union` FOREIGN KEY (`idArea`) REFERENCES `area` (`idArea`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE reserva
+  ADD CONSTRAINT `fk_ID_EMPLEADO_RESERVA` FOREIGN KEY (`idEmpleado`) REFERENCES `empleado` (`idEmpleado`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_ID_AREA_RESERVA` FOREIGN KEY (`idArea`) REFERENCES `area` (`idArea`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
+CREATE TABLE factura(
+    idFactura INT NOT NULL AUTO_INCREMENT,
+    detalle TEXT NOT NULL,
+    nitCliente VARCHAR(10) NOT NULL,
+    fecha DATE NOT NULL,
+    PRIMARY KEY(idFactura)
+);
+
+ALTER TABLE factura
+    ADD KEY `fk_NIT_CLIENTE` (`nitCliente`);
+
+ALTER TABLE factura
+  ADD CONSTRAINT `fk_NIT_CLIENTE_union` FOREIGN KEY (`nitCliente`) REFERENCES `cliente` (`nit`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+CREATE TABLE venta(
+    idVenta INT NOT NULL AUTO_INCREMENT,
+    horas DECIMAL(5,2) NOT NULL,
+    idArea INT NOT NULL,
+    montoParcial DECIMAL(10,2) NOT NULL,
+    descripcion TEXT NOT NULL,
+    idFactura INT NOT NULL,
+    PRIMARY KEY(idVenta)
+);
+
+ALTER TABLE venta
+    ADD KEY `fk_ID_AREA` (`idArea`),
+    ADD KEY `fk_ID_FACTURA` (`idFactura`);
+
+ALTER TABLE venta
+  ADD CONSTRAINT `fk_ID_AREA_VENTA` FOREIGN KEY (`idArea`) REFERENCES `area` (`idArea`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_ID_FACTURA_VENTA` FOREIGN KEY (`idFactura`) REFERENCES `factura` (`idFactura`) ON DELETE NO ACTION ON UPDATE NO ACTION;
